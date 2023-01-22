ec5670260a9881091f554008a0b167394333b961
upfinanceio <upfinance.io@proton.me>
2023-01-22 06:27:48 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,11 +11,14 @@ export function Hero() {
 
       <h3 className='mt-10 mb-0 text-lg text-slate-50'>
         <br></br> Stake to earn monthly rewards in $UP and be eligible for any additional
-        rewards* in different tokens within UP's and our partners' ecosystems... 
-        You have to be staked to earn rewards!!!
+        rewards tokens* that may be added to the contract as part of the monthly distributions.
+        <br></br>
+        You have to be staked to earn rewards!!! Please watch gas price as transactions with multiple
+        NFTs can be costly.  It maybe beneficial to not process more than 50 NFTs at once.
         <br></br>
         <br></br>*Additional reward tokens will vary and will not always be part of monthly
-        distributions.  Active tokens will show in pending rewards.
+        distributions.  Active tokens will show in pending rewards. The contract supports multiple
+        reward tokens. Claiming will claim all tokens at once.
       </h3>
       <img src='./images/logo.png' width='50%' />
     </div>
