87c426a3b92b61c88aafc5048f2f95e595c0d285
upfinanceio <upfinance.io@proton.me>
2023-01-22 06:21:26 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,14 +10,12 @@ export function Hero() {
       </h2>
 
       <h3 className='mt-10 mb-0 text-lg text-slate-50'>
-        You mint your nfts... you then have access to the UP token presale at
-        $1, the token will launch at $1.10 & is 100% backed by BUSD... <br></br>
-        <br></br>You will also get revenue share of everything in the UP
-        ecosystem... It will also give you WL to the top secret things coming
-        soon & more utility in the coming weeks... UP token will be used by
-        other projects launching on BSC which makes it one of the best long term
-        holds... <br></br>
-        <br></br>Keep watching the announcements for more alpha...
+        <br></br> Stake to earn monthly rewards in $UP and be eligible for any additional
+        rewards* in different tokens within UP's and our partners' ecosystems... 
+        You have to be staked to earn rewards!!!
+        <br></br>
+        <br></br>*Additional reward tokens will vary and will not always be part of monthly
+        distributions.  Active tokens will show in pending rewards.
       </h3>
       <img src='./images/logo.png' width='50%' />
     </div>
