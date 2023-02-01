b5c45034a95f72ae31be3e370a0c4e5197137cec
millicare <lacosby.milli@gmail.com>
2023-01-31 16:44:28 -0800

    update nft fetching

--- src/hooks/useUserNFTs.js
+++ src/hooks/useUserNFTs.js
@@ -15,24 +15,36 @@ export default function useUserNFTs(syncFlag) {
   const [userNFTs, setUserNFTs] = useState([]);
 
   useEffect(() => {
-    axios({
-      url: `${MORALIS_API_ENDPOINT}/:address/nft`,
-      method: 'GET',
-      headers: {
-        'x-api-key': MORALIS_API_KEY,
-      },
-      params: {
-        address: address,
-        chain: MORALIS_NETWORK[chainId],
-        token_addresses: nftContract[chainId].address,
-      },
-    })
-      .then((res) => {
-        setUserNFTs(res.data.result.map((token) => token.token_id));
-      })
-      .catch((err) => {
-        console.log(err);
+    const fetchNFTs = async (cursor) => {
+      const res = await axios({
+        url: `${MORALIS_API_ENDPOINT}/:address/nft`,
+        method: 'GET',
+        headers: {
+          'x-api-key': MORALIS_API_KEY,
+        },
+        params: {
+          address: address,
+          chain: MORALIS_NETWORK[chainId],
+          token_addresses: nftContract[chainId].address,
+          cursor,
+        },
       });
+      return res.data;
+    };
+
+    const fetchAll = async () => {
+      let cursor = '';
+      let nfts = [];
+      do {
+        const data = await fetchNFTs(cursor);
+        if (!data) break;
+        cursor = data.cursor;
+        nfts = nfts.concat((data.result || []).map((token) => token.token_id));
+      } while (cursor !== null);
+      setUserNFTs(nfts);
+    };
+
+    fetchAll();
   }, [address, chainId, syncFlag]);
 
   return userNFTs;
