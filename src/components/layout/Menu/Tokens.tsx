cc0304af6478a38e86e0680da15d40d70bac5b0e
upfinanceio <upfinance.io@proton.me>
2023-02-25 20:19:44 -0500

    Update Tokens.tsx

--- src/components/layout/Menu/Tokens.tsx
+++ src/components/layout/Menu/Tokens.tsx
@@ -9,7 +9,7 @@ export default function Tokens() {
 
   const tokens = [
     { id: 1, label: '$UP', link: 'https://app.upfinance.io' },
-    { id: 2, label: '$upRISE', link: 'https://uprise.upfinance.io' },
+    { id: 2, label: '$UP+', link: 'https://upplus.upfinance.io' },
     { id: 3, label: '$upEMP', link: 'https://upemp.upfinance.io/' },
   ];
 
